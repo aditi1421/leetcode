2a41fa4d7e133128bdaca1fbdf1a4636d53f71a3
Aditi Kumar <71980543+aditi1421@users.noreply.github.com>
2023-06-03 12:19:01 +0530

    Update Twosum.py

--- Twosum.py
+++ Twosum.py
@@ -8,3 +8,24 @@ class Solution(object):
             else:
                 return [h[n], i]
   
+  
+  # brute force - 
+  ''' class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                return [i, j] ''' 
+    
+ #hashmap
+ ''' 
+ class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]  
+                ''' 
